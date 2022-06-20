f3b7a89f2aa2e6af3506372a240673cc41116702
margarito <Margarito.jimenez@developers.net>
2022-06-19 19:48:50 -0500

    Changing ADO.NET commands by EntityFramework

--- MSDF.DataChecker.Persistence/Collections/CollectionQueries.cs
+++ MSDF.DataChecker.Persistence/Collections/CollectionQueries.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -61,7 +61,7 @@ namespace MSDF.DataChecker.Persistence.Collections
 
         public async Task<Container> GetByNameAsync(string name)
         {
-            return await _db.Containers.FirstOrDefaultAsync(rec => rec.Name.ToLower() == name);
+            return await _db.Containers.FirstOrDefaultAsync(rec => rec.Name.ToLower() == name.ToLower());
         }
 
         public async Task<List<Container>> GetByRulesDestinationIdAsync(int ruleDestinationId)
