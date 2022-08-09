55dcf2e1af29baf535e2869d0a68e0a80a51c862
Margarito <margarito.jimenez@developers.net>
2022-08-09 17:22:10 -0500

    Config IoC and Installing Postgres packages

--- MSDF.DataChecker.Persistence/Infrastructure/IoC/IoCConfig.cs
+++ MSDF.DataChecker.Persistence/Infrastructure/IoC/IoCConfig.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -6,38 +6,50 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MSDF.DataChecker.Persistence.EntityFramework;
-using System;
 using System.Linq;
-using Hangfire;
-using Hangfire.SqlServer;
+using MSDF.DataChecker.Persistence.Settings;
+using MSDF.DataChecker.Persistence.Providers;
+using System;
 
 namespace MSDF.DataChecker.Persistence.Infrastructure.IoC
 {
     public static class IoCConfig
     {
-        public static void RegisterDependencies(IServiceCollection container, IConfiguration configuration)
+        public static void RegisterDependencies(IServiceCollection container, IConfiguration configuration, IDbAccessProvider dataAccessProvider)
         {
-            container.AddDbContext<DatabaseContext>(options =>options.UseSqlServer(configuration.GetConnectionString("DataCheckerStore")),ServiceLifetime.Transient);
+            configuration.GetEnvironmentVariable();
+            container.Configure<DataBaseSettings>(configuration.GetSection("DatabaseSettings"));
+            var settings = configuration.GetSection("DatabaseSettings").Get<DataBaseSettings>();
+            // Check if is running in Docker
+            if (settings.RunningInDockerContainer)
+            {
+                var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+                connectionString = Utility.ParseConnectionString(connectionString, settings.Engine);
+                if (settings.Engine == "SqlServer")
+                    dataAccessProvider.SQLServer(container, connectionString ?? settings.ConnectionStrings.SqlServer);
+                else
+                    dataAccessProvider.PostgresSQL(container, connectionString ?? settings.ConnectionStrings.PostgresSql);
 
-            container.AddHangfire(globalConfiguration => globalConfiguration
-                  .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-                  .UseSimpleAssemblyNameTypeSerializer()
-                  .UseRecommendedSerializerSettings()
-                  .UseSqlServerStorage(configuration.GetConnectionString("DataCheckerStore"), new SqlServerStorageOptions
-                  {
-                      CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
-                      SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
-                      QueuePollInterval = TimeSpan.Zero,
-                      UseRecommendedIsolationLevel = true,
-                      DisableGlobalLocks = true
-                  }));
-
-            // Add the processing server as IHostedService
-            container.AddHangfireServer();
-
+            }
+            else
+                if (settings.Engine == "SqlServer")
+                dataAccessProvider.SQLServer(container, settings.ConnectionStrings.SqlServer);
+            else
+                dataAccessProvider.PostgresSQL(container, settings.ConnectionStrings.PostgresSql);
             RegisterCommandsAndQueriesByConvention<IPersistenceMarker>(container);
         }
+
+        public static IConfiguration GetEnvironmentVariable(this IConfiguration configuration)
+        {
+            var DOTNET_RUNNING_IN_CONTAINER = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER");
+            var dockerEngine = Environment.GetEnvironmentVariable("Engine");
+            if (!string.IsNullOrEmpty(DOTNET_RUNNING_IN_CONTAINER))
+                configuration.GetSection("DataBaseSettings:RunningInDockerContainer").Value = "true";
+            if (!string.IsNullOrEmpty(dockerEngine))
+                configuration.GetSection("DataBaseSettings:Engine").Value = dockerEngine;
+            return configuration;
+        }
+
 
         private static void RegisterCommandsAndQueriesByConvention<TMarker>(IServiceCollection container)
         {
@@ -63,10 +75,24 @@ namespace MSDF.DataChecker.Persistence.Infrastructure.IoC
                 }
             );
 
+            var providers = (
+             from interfaceType in types.Where(t => t.Name.StartsWith("I") && t.Name.EndsWith("Provider"))
+             from serviceType in types.Where(t => t.Name == interfaceType.Name.Substring(1))
+             select new
+             {
+                 InterfaceType = interfaceType,
+                 ServiceType = serviceType
+             }
+         );
+
+
             foreach (var pair in commandsToRegister)
                 container.AddScoped(pair.InterfaceType, pair.ServiceType);
 
             foreach (var pair in queriesToRegister)
+                container.AddScoped(pair.InterfaceType, pair.ServiceType);
+
+            foreach (var pair in providers)
                 container.AddScoped(pair.InterfaceType, pair.ServiceType);
         }
     }
